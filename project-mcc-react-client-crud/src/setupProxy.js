b12de1f68cdcc30bb9240108c45ada4cbd6bca47
Brian Knauff <bknauff@users.noreply.github.com>
2022-11-02 03:07:00 -0400

    Update setupProxy.js

--- project-mcc-react-client-crud/src/setupProxy.js
+++ project-mcc-react-client-crud/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app){
 	
-	app.use(proxy('/api', { target: '10.107.36.16:8080/' }));
-    app.use(proxy('/account', { target: '10.102.205.230:8081/' }));
+	app.use(proxy('/api', { target: '10.108.147.201:8080/' }));
+    app.use(proxy('/account', { target: '10.99.252.35:8081/' }));
 }
